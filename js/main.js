82755f2605d6ee27fa21d1cd9ac49099bf42f49e
Nícholas Lúcio Castilho <res0077783@aluno.firjansenaisesi.com.br>
2024-09-03 16:48:44 -0300

    segundario commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const url = `https://hp-api.onrender.com/api/characters`
+const harrylistOl= document.getElementById("harrylist")
+
+function convertCharacterToLi(characters){
+    return`
+    <li class ="character">
+    <span> `
+}
